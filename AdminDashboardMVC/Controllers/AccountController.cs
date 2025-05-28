bddbda4f74fda6b22b23163d2ce91ebc7c3428cd
aevangel <aleksandar.evangelatov@progress.com>
2025-05-28 15:00:42 +0300

    chore: update login form

--- AdminDashboardMVC/Controllers/AccountController.cs
+++ AdminDashboardMVC/Controllers/AccountController.cs
@@ -71,9 +71,10 @@ namespace AdminDashboardMVC.Controllers
                 return View(model);
             }
 
+            var isPersistent = model.RememberMe.HasValue && model.RememberMe.Value;
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent, shouldLockout: false);
             
 
             switch (result)
@@ -83,7 +84,7 @@ namespace AdminDashboardMVC.Controllers
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = isPersistent });
                 case SignInStatus.Failure:
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
